47814914c74f3c56d0584a72ed5cfc105171ad8d
Synonymous64 <prajwalurkude007@gmail.com>
2023-01-24 22:50:44 +0530

    final commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,8 +13,27 @@ const CreatePost = () => {
     })
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (form.prompt && form.photo) {
+            setLoading(true);
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/post", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(form)
+                });
 
+                await response.json();
+                navigate("/");
+            } catch (err) {
+                alert(err);
+            } finally {
+                setLoading(false);
+            }
+        } else alert("Enter the prompt and generate an image");
     }
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
@@ -23,8 +42,29 @@ const CreatePost = () => {
         const randomPrompt = getRandomPrompt(form.prompt);
         setForm({ ...form, prompt: randomPrompt })
     }
-    const generateImage = () => {
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setGeneratingImg(true);
+                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ prompt: form.prompt }),
+                });
 
+                const data = await response.json();
+                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+
+            } catch (err) {
+                alert(err);
+            } finally {
+                setGeneratingImg(false);
+            }
+        } else {
+            alert("Please enter the prompt");
+        }
     }
     return (
         <section className='max-w-7x1 mx-auto'>
